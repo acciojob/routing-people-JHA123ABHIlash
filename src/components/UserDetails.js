30f99ff5a826e534a78ec5c8b44ffa44b678eed1
Abhilash Jha <abhilashjha121@gmail.com>
2026-05-18 20:30:31 +0530

    Completed

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,48 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function UserDetails() {
-  const api="https://jsonplaceholder.typicode.com/users";
-  const [ApiData,setApiData]=useState([]);
-  const {id}=useParams();
+  const api = "https://jsonplaceholder.typicode.com/users";
+  const [ApiData, setApiData] = useState([]);
+  const { id } = useParams();
   // console.log(param)
 
-  async function fetchData(){
-    try{
-      const res=await fetch(api);
-       setApiData(await res.json());
-    }catch(err){
-       setApiData([]);
-        console.log(err);
+  async function fetchData() {
+    try {
+      const res = await fetch(api);
+      setApiData(await res.json());
+    } catch (err) {
+      setApiData([]);
+      console.log(err);
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
 
-  const filtered =ApiData.filter((el)=>{
-       return  el.id == id
-      });
-
-      console.log("Filtered Data: ",filtered);
+  const user = ApiData.find((el) => el.id == id);
 
   return (
     <div>
       <h1>User Details</h1>
-      {
-        filtered.map((user)=>(
-          <div key={user.id}>
-        <p>Name: {user.name}</p>
-        <p>Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Phone: {user.phone}</p>
-        <p>Website: {user.website}</p>
-      </div>
-        ))
-      }
-  
-
+      {user && (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
