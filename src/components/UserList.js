7625ba8ac68b9a07a8a77ac89cb07a6cc3c553c2
Abhilash Jha <abhilashjha121@gmail.com>
2026-05-18 20:21:51 +0530

    Completed

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 export default function UserList() {
     const api="https://jsonplaceholder.typicode.com/users";
 
-    const [loading,setLoading]=useState(false);
+    const [loading,setLoading]=useState(true);
     const [data,setData]=useState([]);
 
     console.log(data);
 
     async function fetchData(){
         
-              setLoading(true);
         try {
             const res=await fetch(api);
              setData(await res.json());
